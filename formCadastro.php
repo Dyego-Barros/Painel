5de1b083281378d0fa741de6eecad103e6a9be1f
Dyego-Barros <gabutdd@gmail.com>
2017-06-16 00:11:58 -0300

    Painel-funcionando

---
+++ formCadastro.php
@@ -0,0 +1,63 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+ <title>Template HTML5</title> 
+     
+     <meta name="viewport" content="width-device-width initial-scale-1.0">
+     <meta chasert="UTF-8">
+    <title>Template HTML5</title>
+    
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+     <link href="css/bootstrap.min.css" rel="stylesheet" media="all">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+     <link href="css/painel.css" rel="stylesheet" media="all">
+     
+</head>
+ <body data-spy="scroll" data-target=".menu-navegacao" data-offset="80">
+  
+   <div class="container">
+   	<div class="row">
+   		<div class="col-xs-12">
+   			<form>
+   			<div class="col-xs-6">
+   				<div class="input-group ">
+                            <span class="input-group-addon"><i class="fa fa-user-md" aria-hidden="true"></i></span>
+                            <input  type="email" class="form-control" name="email" value="" placeholder="Nome do Médico">                                        
+                 </div>
+   				
+   				</div>
+   				
+   				<div class="col-xs-6">
+   				<div class="input-group ">
+                            <span class="input-group-addon"><i class="fa fa-h-square" aria-hidden="true"></i></span>
+                            <input  type="number" class="form-control" name="crm" value="" placeholder="CRM">                                        
+                 </div>
+   				
+   				</div>
+   				
+   				<div class="col-xs-6">
+   				<div class="input-group ">
+                            <span class="input-group-addon"><i class="fa fa-hospital-o" aria-hidden="true"></i></span>
+                            <input  type="text" class="form-control" name="nome-clinica" value="" placeholder="Insira o nome da clinica">                                        
+                 </div>
+   				
+   				</div>
+   				
+   					<div class="col-xs-6">
+   				<div class="input-group ">
+                            <span class="input-group-addon"><i class="fa fa-mobile" aria-hidden="true"></i></span>
+                            <input  type="tel" class="form-control" name="tel" value="" placeholder="Insira o telefone do Médico">                                        
+                 </div>
+   				
+   				</div>
+   				
+   			</form>
+   			
+   		</div>   		
+   	</div> 
+   </div>
+   
+ 
+</body>
+</html>
